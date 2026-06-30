ef8272dc6cc679a9aa3f7100569da17bf3b3c30f
Anilkumar64 <vennapureddyanil456@gmail.com>
2026-06-30 16:25:26 +0530

    added contact header file with all its paramerts

---
+++ include/Contact.h
@@ -0,0 +1,29 @@
+
+#ifndef CONTACT_MANAGER_CONTACT_H
+#define CONTACT_MANAGER_CONTACT_H
+#include <string>
+#include <array>
+class Contact
+{
+private:
+    std::string Name_;
+    std::string PhoneNumber_;
+    std::string Email_;
+public:
+    Contact(std::string Name,std::string P_No,std::string Email):Name_(Name),PhoneNumber_(P_No),Email_(Email){};
+    std::string getName () const
+    {
+        return Name_;
+    }
+    std::string getPhone() const
+    {
+        return PhoneNumber_;
+    }
+    std::string getEmail() const
+    {
+        return Email_;
+    }
+
+};
+
+#endif //CONTACT_MANAGER_CONTACT_H
