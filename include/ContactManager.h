5e65618f4d0c67d0d1de0f137a6debfe5e41a2ab
Anilkumar64 <vennapureddyanil456@gmail.com>
2026-06-30 16:41:17 +0530

    Reconfured the data for contact management with all its funtions

--- include/ContactManager.h
+++ include/ContactManager.h
@@ -4,5 +4,19 @@
 
 #ifndef CONTACT_MANAGER_CONTACTMANAGER_H
 #define CONTACT_MANAGER_CONTACTMANAGER_H
+#include "Contact.h"
+
+class ContactManager
+{
+private:
+    vector<Contact> &contacts;
+public:
+    void addContact(const Contact& contact);
+    void removeContact(const Contact &contact);
+    void listall() const;
+    void savetoFile(const std::string &file_name) const;
+    void loadFromFile(const std::string &file_name) const;
+
+};
 
 #endif //CONTACT_MANAGER_CONTACTMANAGER_H
