0742e11504a8946dcda8b3ec8675a26fb11dd5a8
Anilkumar64 <vennapureddyanil456@gmail.com>
2026-07-01 06:45:24 +0530

    resetructing the main.cppp now  with necessatiy chnages in the work relatead to contactmanager

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
+#include "include/Contact.h"
+#include "include/ContactManager.h"
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    ContactManager manager;
+    int choice{0};
+    while (choice !=6)
+    {
+        std::cout << "\n--- Contact Manager ---\n";
+        std::cout << "1. Add Contact\n";
+        std::cout << "2. Remove Contact\n";
+        std::cout << "3. List All Contacts\n";
+        std::cout << "4. Save to File\n";
+        std::cout << "5. Load from File\n";
+        std::cout << "6. Exit\n";
+        std::cout << "Enter choice: ";
+        std::cin >> choice;
+        if (choice == 1)
+        {
+            std ::cout <<"Enter the Name : " ;
+            std ::cout << std::endl;
+            std::cout << "Enter the PhoneNumber :"
+            std::string name, phonenumber , email;
+            std::cin >> name;
+        }
+    }
+
+
 
 }
